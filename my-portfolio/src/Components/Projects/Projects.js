e546d59f15bf63a0f37703e916dbf74868cdc69a
Mirza Sisic <mirzasisic57@gmail.com>
2018-04-17 14:40:42 -0400

    Images

--- my-portfolio/src/Components/Projects/Projects.js
+++ my-portfolio/src/Components/Projects/Projects.js
@@ -6,22 +6,22 @@ const Projects = () => (
     <h1>My Portfolio</h1>
 
     <div class="col-lg-4 col-md-4 col-sm-4 gallery">
-      <a href="work.html"><img src="assets/img/portfolio/folio01.png" class="img-responsive"/></a>
+      <a href="work.html"><img src={require("src/Images/portfolio/folio01.png")} class="img-responsive"/></a>
     </div>
     <div class="col-lg-4 col-md-4 col-sm-4 gallery">
-      <a href="work.html"><img src="assets/img/portfolio/folio02.png" class="img-responsive"/></a>
+      <a href="work.html"><img src="../src/images/portfolio/folio02.png" class="img-responsive"/></a>
     </div>
     <div class="col-lg-4 col-md-4 col-sm-4 gallery">
-      <a href="work.html"><img src="assets/img/portfolio/folio03.png" class="img-responsive"/></a>
+      <a href="work.html"><img src="../src/images/portfolio/folio03.png" class="img-responsive"/></a>
     </div>
     <div class="col-lg-4 col-md-4 col-sm-4 gallery">
-      <a href="work.html"><img src="assets/img/portfolio/folio04.png" class="img-responsive"/></a>
+      <a href="work.html"><img src="../src/images/portfolio/folio04.png" class="img-responsive"/></a>
     </div>
     <div class="col-lg-4 col-md-4 col-sm-4 gallery">
-      <a href="work.html"/><img src="assets/img/portfolio/folio05.png" class="img-responsive"/><a/>
+      <a href="work.html"/><img src="../src/images/portfolio/folio05.png" class="img-responsive"/><a/>
     </div>
     <div class="col-lg-4 col-md-4 col-sm-4 gallery">
-      <a href="work.html"><img src="assets/img/portfolio/folio06.png" class="img-responsive"/></a>
+      <a href="work.html"><img src="../src/images/portfolio/folio06.png" class="img-responsive"/></a>
     </div>
   </div>
 </div>
