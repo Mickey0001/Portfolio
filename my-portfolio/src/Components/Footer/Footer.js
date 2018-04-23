920bad185c0dc07df29f0c15582ca71a5589ea34
Mirza Sisic <mirzasisic57@gmail.com>
2018-04-23 12:04:22 -0400

    Menu Correction

--- my-portfolio/src/Components/Footer/Footer.js
+++ my-portfolio/src/Components/Footer/Footer.js
@@ -11,14 +11,8 @@ const Footer = () => (
 					<p id="copyright">	&copy;<span>{today.getFullYear()}&nbsp;mirzasisic.com</span></p>
 				</div>
 				<div className="row pull-right">
-						 <div className="col-lg-2 col-md-2 col-xs-1">
-								<a href="https://github.com/Mickey0001" target="_blank" rel="noopener noreferrer"><i className="fa fa-github fa-1x"></i></a>
-							</div>
 							<div className="col-lg-2 col-md-2 col-xs-1">
 								<a href="https://stackoverflow.com/users/story/7443287" target="_blank" rel="noopener noreferrer"><i className="fa fa-stack-overflow  fa-1x"></i></a>
-							</div>
-							<div className="col-lg-2 col-md-2 col-xs-1">
-								<a href="https://www.linkedin.com/in/sisicmirza" target="_blank" rel="noopener noreferrer"><i className="fa fa-linkedin fa-1x"></i></a>
 							</div>
 							<div className="col-lg-2 col-md-2 col-xs-1">
 								<a href="https://www.facebook.com/juniorlearnstocode/" target="_blank" rel="noopener noreferrer"><i className="fa fa-facebook fa-1x"></i></a>
@@ -27,8 +21,14 @@ const Footer = () => (
 								<a href="https://twitter.com/MickeySisic" target="_blank" rel="noopener noreferrer"><i className="fa fa-twitter fa-1x"></i></a>
 							</div>
 							<div className="col-lg-2 col-md-2 col-xs-1">
-								<a href="https://www.instagram.com/mirzasisic0001" target="_blank" rel="noopener noreferrer"><i className="fa fa-instagram fa-1x"></i></a>
-							</div>			
+								<a href="https://www.linkedin.com/in/sisicmirza" target="_blank" rel="noopener noreferrer"><i className="fa fa-linkedin fa-1x"></i></a>
+							</div>
+							<div className="col-lg-2 col-md-2 col-xs-1">
+								<a href="https://github.com/Mickey0001" target="_blank" rel="noopener noreferrer"><i className="fa fa-github fa-1x"></i></a>
+							</div>
+							<div className="col-lg-2 col-md-2 col-xs-1">
+								<a href="https://plus.google.com/u/0/+mirzasisic" target="_blank" rel="noopener noreferrer"><i className="fa fa-google-plus fa-1x"></i></a>
+							</div>
 					</div>		
 			</div>		
 		</div>
