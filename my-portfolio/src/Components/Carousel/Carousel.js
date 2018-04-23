7a053e6b6cff83608e1adb7a42c488cd2d9b03ba
Mirza Sisic <mirzasisic57@gmail.com>
2018-04-23 12:42:13 -0400

    Carousel Placeholder Slides

--- my-portfolio/src/Components/Carousel/Carousel.js
+++ my-portfolio/src/Components/Carousel/Carousel.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import Carousel from 'nuka-carousel';
 
-const Carousel = () => (
-  <div>
-       <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide1" />
-        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide2" />
-        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide3" />
-        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide4" />
-        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide5" />
-        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide6" />
-</div>
-);
-
-export default Carousel;
+export default class extends React.Component {
+  render() {
+    return (
+      <Carousel className="text-center">
+          <div><img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide1"/></div>
+          <div><img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide2"/></div>
+          <div><img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide3"/></div>
+          <div><img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide4"/></div>
+          <div><img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide5"/></div>
+          <div><img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide6"/></div>
+      </Carousel>
+    );
+  }
+}
