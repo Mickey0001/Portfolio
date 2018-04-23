8ecb26aa228f93752c2c5a94cd274cb980df10a6
Mirza Sisic <mirzasisic57@gmail.com>
2018-04-23 12:59:01 -0400

    Recommendations

--- my-portfolio/src/Components/Carousel/Carousel.js
+++ my-portfolio/src/Components/Carousel/Carousel.js
@@ -4,8 +4,12 @@ import Carousel from 'nuka-carousel';
 export default class extends React.Component {
   render() {
     return (
-      <Carousel className="text-center">
-          <div><img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide1"/></div>
+      <Carousel id="carousel" className="text-center">
+          <div className="carousel-item">
+             <p className="text-center">"It was wonderful to work with Mirza. Efficient, skilled, was fast, easy going, fast at responding to my requests, managed to learn how to work things out and communicated his progress and challenges. I will hire him again and fully recommend. Thank you so much for your reliability, professionalism and caring nature."</p>
+            <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide1"/>
+            <p>- Dilma Morais <br/> Upwork Client</p>
+            </div>
           <div><img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide2"/></div>
           <div><img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide3"/></div>
           <div><img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide4"/></div>
