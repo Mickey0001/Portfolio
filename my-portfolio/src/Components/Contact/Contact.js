d70d4fe92dd74b9c9e18afd76b70e9798a2e13e1
Mirza Sisic <mirzasisic57@gmail.com>
2018-05-02 10:05:14 -0400

    npm install redux

--- my-portfolio/src/Components/Contact/Contact.js
+++ my-portfolio/src/Components/Contact/Contact.js
@@ -1,59 +1,106 @@
 import React from 'react';
-import { FormGroup, FormControl, ControlLabel, HelpBlock } from 'react-bootstrap';
+import { Field, reduxForm } from 'redux-form';
 
-class FormExample extends React.Component {
-  constructor(props, context) {
-    super(props, context);
-
-    this.handleChange = this.handleChange.bind(this);
-
-    this.state = {
-      value: ''
-    };
-  }
-
-  getValidationState() {
-    const length = this.state.value.length;
-    if (length > 10) return 'success';
-    else if (length > 5) return 'warning';
-    else if (length > 0) return 'error';
-    return null;
-  }
-
-  handleChange(e) {
-    this.setState({ value: e.target.value });
-  }
-
-  render() {
-    return (
-      <form className="text-center" id="Contact">
-        <FormGroup
-          controlId="formBasicText"
-          validationState={this.getValidationState()}
-        >
-          <ControlLabel>Send me an email</ControlLabel>
-          <FormControl 
-            className="Form"
+const SimpleForm = props => {
+  const { handleSubmit, pristine, reset, submitting } = props
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>First Name</label>
+        <div>
+          <Field
+            name="firstName"
+            component="input"
             type="text"
-            value={this.state.value}
-            placeholder="Enter text"
-            onChange={this.handleChange}
+            placeholder="First Name"
           />
-           <ControlLabel></ControlLabel>
-          <FormControl 
-            className="Form"
+        </div>
+      </div>
+      <div>
+        <label>Last Name</label>
+        <div>
+          <Field
+            name="lastName"
+            component="input"
             type="text"
-            value={this.state.value}
-            placeholder="Enter text"
-            onChange={this.handleChange}
+            placeholder="Last Name"
           />
-          <FormControl.Feedback />
-          <HelpBlock>Or connect with me over social media.</HelpBlock>
-        </FormGroup>
-      </form>
-    );
-  }
+        </div>
+      </div>
+      <div>
+        <label>Email</label>
+        <div>
+          <Field
+            name="email"
+            component="input"
+            type="email"
+            placeholder="Email"
+          />
+        </div>
+      </div>
+      <div>
+        <label>Sex</label>
+        <div>
+          <label>
+            <Field
+              name="sex"
+              component="input"
+              type="radio"
+              value="male"
+            />{' '}
+            Male
+          </label>
+          <label>
+            <Field
+              name="sex"
+              component="input"
+              type="radio"
+              value="female"
+            />{' '}
+            Female
+          </label>
+        </div>
+      </div>
+      <div>
+        <label>Favorite Color</label>
+        <div>
+          <Field name="favoriteColor" component="select">
+            <option />
+            <option value="ff0000">Red</option>
+            <option value="00ff00">Green</option>
+            <option value="0000ff">Blue</option>
+          </Field>
+        </div>
+      </div>
+      <div>
+        <label htmlFor="employed">Employed</label>
+        <div>
+          <Field
+            name="employed"
+            id="employed"
+            component="input"
+            type="checkbox"
+          />
+        </div>
+      </div>
+      <div>
+        <label>Notes</label>
+        <div>
+          <Field name="notes" component="textarea" />
+        </div>
+      </div>
+      <div>
+        <button type="submit" disabled={pristine || submitting}>
+          Submit
+        </button>
+        <button type="button" disabled={pristine || submitting} onClick={reset}>
+          Clear Values
+        </button>
+      </div>
+    </form>
+  )
 }
-(<FormExample />);
 
-export default FormExample;
+export default reduxForm({
+  form: 'simple' // a unique identifier for this form
+})(SimpleForm)
