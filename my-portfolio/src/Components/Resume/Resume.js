2823b6d6cc686f77e06d768275e061bc5052af63
Mirza Sisic <mirzasisic57@gmail.com>
2018-05-07 16:17:21 -0400

    Resume CSS

--- my-portfolio/src/Components/Resume/Resume.js
+++ my-portfolio/src/Components/Resume/Resume.js
@@ -1,51 +1,51 @@
 import React from 'react';
 
 const Resume = () => (
-<div id="Resume" class="container">
-<div class="row">
+<div id="Resume" className="container">
+<div className="row">
 <h3 className="text-center heading"><strong>Resume</strong></h3>
-    <ul class="timeline">
+    <ul className="timeline">
         <li>
-          <div class="timeline-badge"><i class="glyphicon glyphicon-check"></i></div>
-          <div class="timeline-panel">
-            <div class="timeline-heading">
-              <h4 class="timeline-title">Mussum ipsum cacilds</h4>
-              <p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> 11 hours ago via Twitter</small></p>
+          <div className="timeline-badge"><i className="glyphicon glyphicon-check"></i></div>
+          <div className="timeline-panel">
+            <div className="timeline-heading">
+              <h4 className="timeline-title">Mussum ipsum cacilds</h4>
+              <p><small className="text-muted"><i className="glyphicon glyphicon-time"></i> 11 hours ago via Twitter</small></p>
             </div>
-            <div class="timeline-body">
+            <div className="timeline-body">
               <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
             </div>
           </div>
         </li>
         <li>
-          <div class="timeline-badge danger"><i class="glyphicon glyphicon-credit-card"></i></div>
-          <div class="timeline-panel">
-            <div class="timeline-heading">
-              <h4 class="timeline-title">Mussum ipsum cacilds</h4>
+          <div className="timeline-badge danger"><i className="glyphicon glyphicon-credit-card"></i></div>
+          <div className="timeline-panel">
+            <div className="timeline-heading">
+              <h4 className="timeline-title">Mussum ipsum cacilds</h4>
             </div>
-            <div class="timeline-body">
+            <div className="timeline-body">
               <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
             </div>
           </div>
         </li>
         <li>
-          <div class="timeline-badge info"><i class="glyphicon glyphicon-floppy-disk"></i></div>
-          <div class="timeline-panel">
-            <div class="timeline-heading">
-              <h4 class="timeline-title">Mussum ipsum cacilds</h4>
+          <div className="timeline-badge info"><i className="glyphicon glyphicon-floppy-disk"></i></div>
+          <div className="timeline-panel">
+            <div className="timeline-heading">
+              <h4 className="timeline-title">Mussum ipsum cacilds</h4>
             </div>
-            <div class="timeline-body">
+            <div className="timeline-body">
               <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
               <hr/>
-              <div class="btn-group">
-                <button type="button" class="btn btn-primary btn-sm dropdown-toggle" data-toggle="dropdown">
-                  <i class="glyphicon glyphicon-cog"></i> <span class="caret"></span>
+              <div className="btn-group">
+                <button type="button" className="btn btn-primary btn-sm dropdown-toggle" data-toggle="dropdown">
+                  <i className="glyphicon glyphicon-cog"></i> <span className="caret"></span>
                 </button>
-                <ul class="dropdown-menu" role="menu">
+                <ul className="dropdown-menu" role="menu">
                   <li><a href="#">Action</a></li>
                   <li><a href="#">Another action</a></li>
                   <li><a href="#">Something else here</a></li>
-                  <li class="divider"></li>
+                  <li className="divider"></li>
                   <li><a href="#">Separated link</a></li>
                 </ul>
               </div>
@@ -53,11 +53,11 @@ const Resume = () => (
           </div>
         </li>
         <li>
-          <div class="timeline-panel">
-            <div class="timeline-heading">
-              <h4 class="timeline-title">Mussum ipsum cacilds</h4>
+          <div className="timeline-panel">
+            <div className="timeline-heading">
+              <h4 className="timeline-title">Mussum ipsum cacilds</h4>
             </div>
-            <div class="timeline-body">
+            <div className="timeline-body">
               <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
             </div>
           </div>
