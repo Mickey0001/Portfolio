dca77390a1949cd56e77d46754e5f9365f8dfd16
Mirza Sisic <mirzasisic57@gmail.com>
2018-05-08 13:25:14 -0400

    Contact button

--- my-portfolio/src/Components/Contact/Contact.js
+++ my-portfolio/src/Components/Contact/Contact.js
@@ -1,9 +1,8 @@
 import React from 'react';
 
 const Contact = () => (
-	<div id="Contact" className="container">
-
-   
+	<div id="Contact" className="container text-center">
+    <button className="btn btn-basic">Email me</button>
 	</div>
 );
 
