7399e3b78c44c8b4a0fb2a7c84d2c0f56450940e
Mirza Sisic <mirzasisic57@gmail.com>
2018-08-08 19:34:42 +0200

    Update Mail Subject

--- my-portfolio/src/Components/Contact/Contact.js
+++ my-portfolio/src/Components/Contact/Contact.js
@@ -4,7 +4,7 @@ const Contact = () => (
 	<div id="Contact" className="container text-center">
     <h3 className="text-center heading"><strong>Contact</strong></h3>
     <h4 className="text-center  minor-heading">Send me an email.</h4>
-    <a href="mailto:mirzasisic57@gmail.com?Subject=Website%20visitor" target="_top">
+    <a href="mailto:mirzasisic57@gmail.com?Subject=MirzaSisic.com%20Visitor" target="_top">
     <i className="fa fa-envelope-o fa-5x email" aria-hidden="true">
     </i></a><br/>
     <h4 className="text-center  minor-heading">Or connect with me over social media.</h4>
