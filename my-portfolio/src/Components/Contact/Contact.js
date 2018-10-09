220c68eeb9627b9b17f35fe35cf755ab661a95cf
Mirza Sisic <mirzasisic57@gmail.com>
2018-10-09 17:09:25 +0200

    Rearrange Social Media

--- my-portfolio/src/Components/Contact/Contact.js
+++ my-portfolio/src/Components/Contact/Contact.js
@@ -7,7 +7,43 @@ const Contact = () => (
     <a href="mailto:mirzasisic57@gmail.com?Subject=MirzaSisic.com%20Visitor" target="_top">
     <i className="fa fa-envelope-o fa-5x email" aria-hidden="true">
     </i></a><br/>
-    <h4 className="text-center  minor-heading">Or connect with me over social media.</h4>
+    <h4 className="text-center  minor-heading">Or connect with me <br/> over social media.</h4>
+    <div className="row social">
+							<div>
+								<a className="social-icons" title="GitHub" href="https://github.com/Mickey0001" target="_blank" rel="noopener noreferrer"><i className="fa fa-github fa-2x"></i></a>
+							</div>
+                            <div>
+								<a className="social-icons" title="CodePen" href="https://codepen.io/mickey0001/" target="_blank" rel="noopener noreferrer"><i className="fa fa-codepen fa-2x"></i></a>
+							</div>
+							<div>
+								<a className="social-icons" title="LinkedIn" href="https://www.linkedin.com/in/sisicmirza" target="_blank" rel="noopener noreferrer"><i className="fa fa-linkedin fa-2x"></i></a>
+							</div>
+                            <div>
+								<a className="social-icons" title="Free Code Camp" href="https://www.freecodecamp.org/portfolio/mickey0001" target="_blank" rel="noopener noreferrer"><i className="fa fa-free-code-camp fa-2x"></i></a>
+							</div>
+                            <div>
+								<a className="social-icons" title="Stack Overflow" href="https://stackoverflow.com/users/7443287/mirza-sisic?tab=profile" target="_blank" rel="noopener noreferrer"><i className="fa fa-stack-overflow fa-2x"></i></a>
+							</div>
+					</div>	
+                    <br/>	
+                    <div className="row social">
+                     
+                            <div>
+								<a className="social-icons" title="Twitter" href="https://twitter.com/MickeySisic" target="_blank" rel="noopener noreferrer"><i className="fa fa-twitter fa-2x"></i></a>
+							</div>
+                            <div>
+								<a className="social-icons" title="Facebook" href="https://www.facebook.com/juniorlearnstocode/" target="_blank" rel="noopener noreferrer"><i className="fa fa-facebook fa-2x"></i></a>
+							</div>
+							<div>
+								<a className="social-icons" title="Free Code Camp" href="https://www.pinterest.co.uk/mirzasisic57" target="_blank" rel="noopener noreferrer"><i className="fa fa-pinterest-p fa-2x"></i></a>
+							</div>
+                            <div>
+								<a className="social-icons" title="Google Plus" href="https://plus.google.com/u/0/+mirzasisic" target="_blank" rel="noopener noreferrer"><i className="fa fa-google-plus fa-2x"></i></a>
+							</div>
+							<div>
+								<a className="social-icons" title="Free Code Camp" href="https://www.instagram.com/mirzasisic0001" target="_blank" rel="noopener noreferrer"><i className="fa fa-instagram fa-2x"></i></a>
+							</div>
+                    </div>
 	</div>
 );
 
--- my-portfolio/src/Components/Footer/Footer.js
+++ my-portfolio/src/Components/Footer/Footer.js
@@ -9,36 +9,7 @@ const Footer = () => (
 			<div className="row centered">
 				<div className="">
 				<img alt="Mirza Sisic logo" src={Logo}/>
-				</div>
-				<div className="row social">
-							<div>
-								<a className="social-icons" title="GitHub" href="https://github.com/Mickey0001" target="_blank" rel="noopener noreferrer"><i className="fa fa-github"></i></a>
-							</div>
-							<div>
-								<a className="social-icons" title="Twitter" href="https://twitter.com/MickeySisic" target="_blank" rel="noopener noreferrer"><i className="fa fa-twitter"></i></a>
-							</div>
-							<div>
-								<a className="social-icons" title="LinkedIn" href="https://www.linkedin.com/in/sisicmirza" target="_blank" rel="noopener noreferrer"><i className="fa fa-linkedin"></i></a>
-							</div>
-							<div>
-								<a className="social-icons" title="Facebook" href="https://www.facebook.com/juniorlearnstocode/" target="_blank" rel="noopener noreferrer"><i className="fa fa-facebook"></i></a>
-							</div>
-							<div>
-								<a className="social-icons" title="Google Plus" href="https://plus.google.com/u/0/+mirzasisic" target="_blank" rel="noopener noreferrer"><i className="fa fa-google-plus"></i></a>
-							</div>
-							<div>
-								<a className="social-icons" title="Stack Overflow" href="https://stackoverflow.com/users/7443287/mirza-sisic?tab=profile" target="_blank" rel="noopener noreferrer"><i className="fa fa-stack-overflow"></i></a>
-							</div>
-							<div>
-								<a className="social-icons" title="CodePen" href="https://codepen.io/mickey0001/" target="_blank" rel="noopener noreferrer"><i className="fa fa-codepen"></i></a>
-							</div>
-							<div>
-								<a className="social-icons" title="Free Code Camp" href="https://www.freecodecamp.org/portfolio/mickey0001" target="_blank" rel="noopener noreferrer"><i className="fa fa-free-code-camp"></i></a>
-							</div>
-							<div>
-								<a className="social-icons" title="Free Code Camp" href="https://www.pinterest.co.uk/mirzasisic57" target="_blank" rel="noopener noreferrer"><i className="fa fa-pinterest-p"></i></a>
-							</div>
-					</div>		
+				</div><br/>
 			</div>		
 		</div>
 		<p id="copyright" className="text-center">	&copy;<span>{today.getFullYear()}&nbsp;mirzasisic.com</span> <br/>Powered by React</p>
