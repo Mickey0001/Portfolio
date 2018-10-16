483ce1ca83c25d82946734d7c1a560694d1daeca
Mirza Sisic <mirzasisic57@gmail.com>
2018-10-16 21:08:27 +0200

    Add <br/> for sentences

--- my-portfolio/src/Components/Testimonials/Testimonials.js
+++ my-portfolio/src/Components/Testimonials/Testimonials.js
@@ -22,7 +22,7 @@ const Testimonials = () => (
           </div>
 
           <div className="carousel-item col-md-12 col-sm-12" align="center">
-              <p className="text-center ClientQuote">"It was wonderful to work with Mirza.  Thank you so much for your reliability, professionalism and caring nature."</p>
+              <p className="text-center ClientQuote">"It was wonderful to work with Mirza. <br/>  Thank you so much for your reliability, professionalism and caring nature."</p>
               <img className="ClientImage" alt="Dilma Morais"  src={Dilma}/>
               <p className="ClientName">Dilma Morais<br/><span className="ClientInfo">Upwork Client</span></p>
           </div>
@@ -30,13 +30,13 @@ const Testimonials = () => (
 
         <div className="row">
         <div className="carousel-item col-md-12 col-sm-12" align="center">
-                    <p className="text-center ClientQuote">"Punctual and emphatic towards people who are not too tech savvy. Mirza was very kind and pleasant to work with."</p>
+                    <p className="text-center ClientQuote">"Punctual and emphatic towards people who are not too tech savvy. <br/> Mirza was very kind and pleasant to work with."</p>
                     <img className="ClientImage" alt="Dijana Lekic" src={Dijana}/>
                     <p className="ClientName">Dijana Lekic<br/><span className="ClientInfo">MSc in Chemistry</span></p>
                 </div>
 
                 <div className="carousel-item col-md-12 col-sm-12" align="center">
-                    <p className="text-center ClientQuote">"I have to say, if you want a professional looking website, Mirza is the guy you're looking for."</p>
+                    <p className="text-center ClientQuote">"If you want a professional looking website <br/> Mirza is the guy you're looking for."</p>
                     <img className="ClientImage" alt="Zoran Milanovic" src={Zoran}/>
                     <p className="ClientName">Zoran Milanovic<br/><span className="ClientInfo">eCommerce Owner</span></p>
                 </div>
@@ -44,7 +44,7 @@ const Testimonials = () => (
 
             <div className="row">
             <div className="carousel-item col-md-12 col-sm-12" align="center">
-                        <p className="text-center ClientQuote">"It was a pleasing experience working together. Everything was done according to the instructions and in timely manner. Recommended!"</p>
+                        <p className="text-center ClientQuote">"It was a pleasing experience working together. <br/> Everything was done according to the instructions and in timely manner. <br/>Recommended!"</p>
                         <img className="ClientImage" alt="Emilis Strimaitis" src={Emilis}/>
                         <p className="ClientName">Emilis Strimaitis<br/><span className="ClientInfo">Upwork Client</span></p>
                     </div>
@@ -73,13 +73,13 @@ const Testimonials = () => (
 
                 <div className="row">
                 <div className="carousel-item col-md-12 col-sm-12" align="center">
-                            <p className="text-center ClientQuote">"The job was finished successfully. I am satisfied with that."</p>
+                            <p className="text-center ClientQuote">"The job was finished successfully.<br/> I am satisfied with that."</p>
                             <img className="ClientImage" alt="David Gao" src={David}/>
                             <p className="ClientName">David Gao<br/><span className="ClientInfo">Upwork Client</span></p>
                         </div>
 
                         <div className="carousel-item col-md-12 col-sm-12" align="center">
-                            <p className="text-center ClientQuote">"It was a pleasure with Mirza. He did his work extremely well and he did good job on our WordPress Theme from scratch. I'm looking forward working with him again."</p>
+                            <p className="text-center ClientQuote">"It was a pleasure with Mirza.<br/> He did his work extremely well and he did good job on our WordPress theme from scratch.<br/> I'm looking forward working with him again."</p>
                             <img className="ClientImage" alt="Gamaliel Rausch" src={Gamaliel}/>
                             <p className="ClientName">Gamaliel Rausch<br/><span className="ClientInfo">Upwork Client</span></p>
                         </div>
@@ -88,7 +88,7 @@ const Testimonials = () => (
             <div className="row">
             
             <div className="carousel-item col-md-12 col-sm-12" align="center">
-                    <p className="text-center ClientQuote">"I loved working with Mirza. A charming guy, always available and easy to work with. The project was seamless and sounded effortless. He knows his stuff."</p>
+                    <p className="text-center ClientQuote">"I loved working with Mirza.<br/> A charming guy, always available and easy to work with.<br/> The project was seamless and sounded effortless. <br/>He knows his stuff."</p>
                     <img className="ClientImage" alt="Rebecca Chalach" src={Rebecca}/>
                     <p className="ClientName">Rebecca Chalach<br/><span className="ClientInfo">Upwork Client</span></p>
                 </div>
