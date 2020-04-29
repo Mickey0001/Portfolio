e8939fe8e014b41b649e06c48cf89bdadece64b3
MirzaSisic <mirzasisic57@gmail.com>
2020-04-29 17:20:59 +0200

    Add new Portfolio proj for test

---
+++ Portfolio/Class1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Interfaces.Streaming;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Portfolio
+{
+    public class Class1
+    {
+    }
+}
