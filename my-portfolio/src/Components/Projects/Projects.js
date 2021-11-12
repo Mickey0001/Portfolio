8cd83e5f0bfe1bdec89a5a51bc9520d31eff7ee9
Mirza Sisic <84443026+mirzonius@users.noreply.github.com>
2021-11-12 11:52:06 +0100

    Add images for two MoT articles

--- my-portfolio/src/Components/Projects/Projects.js
+++ my-portfolio/src/Components/Projects/Projects.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import SafetBeg from '../../../src/Images/SafetBeg.jpg';
-import Dijana from '../../../src/Images/Dijana.science.jpg';
+import LeadershipLessons from '../../../src/Images/LeassonsLearnedAboutLeadershipMotArticle.png';
+import ExploratoryTools from '../../../src/Images/ExploratoryToolsForBetterTesting.jpg';
 import Basheskia from '../../../src/Images/BasheskiaEdward.jpg';
 import BuvaShop from '../../../src/Images/BuvaShop.jpg';
 import Amuleto from '../../../src/Images/AmuletoDesigns.jpg';
@@ -10,28 +10,28 @@ import Fence from '../../../src/Images/EasyFenceFix.jpg';
 
 const Projects = () => (
   <div className="container" id="workwrap">
-  <h3 className="text-center heading"><strong>Projects</strong></h3>
-  <h4 className="minor-heading"><strong>Sites made by me</strong></h4>
+  <h3 className="text-center heading"><strong>My Work</strong></h3>
+  <h4 className="minor-heading"><strong>What I've been up to lately</strong></h4>
   
     <div className="row">
         <div className="col-sm-6 col-md-3">
             <div className="thumbnail">
-              <img className="img-responsive project" src={SafetBeg} alt="Painting by Safet Begic"/>
+              <img className="img-responsive project" src={LeadershipLessons} alt="Lessons Learned About Leadership, From Perspective Of An Aspiring Leader - For testers trying to get into Team Lead or similar roles, article by Mirza Sisic"/>
               <div className="caption">
-                <h3>Safetbeg.com</h3>
-                <p className="DarkGreyHeading">Artist Portfolio</p>
-                <p><a href="https://safetbeg.com/" className="btn btn-default LightGreyBackground" target="_blank" rel="noopener noreferrer" role="button">Live Site</a> <a href="https://github.com/Mickey0001/SafetBeg/" target="_blank" rel="noopener noreferrer" className="btn btn-default DarkGreyBackground" role="button">View Code</a></p>
+                <h3>Lessons Learned About Leadership, From Perspective Of An Aspiring Leader</h3>
+                <p className="DarkGreyHeading">Ministry of Testing - The Testing Planet 2021</p>
+                <p><a href="https://www.ministryoftesting.com/dojo/lessons/lessons-learned-about-leadership-from-perspective-of-an-aspiring-leader" className="btn btn-default LightGreyBackground" target="_blank" rel="noopener noreferrer" role="button">Read the article</a></p>
               </div>
             </div>
           </div>
 
           <div className="col-sm-6 col-md-3">
             <div className="thumbnail">
-              <img className="img-responsive project" src={Dijana} alt="Dijana science"/>
+              <img className="img-responsive project" src={ExploratoryTools} alt="Software Tools For Easy, Effective Exploratory Testing, article by Mirza Sisic"/>
               <div className="caption">
-                <h3>Dijana.Science</h3>
-                <p className="DarkGreyHeading">Chemist Portfolio</p>
-                <p><a href=" https://mickey0001.github.io/Dixie" className="btn btn-default LightGreyBackground" target="_blank" rel="noopener noreferrer" role="button">Live Site</a> <a href="https://github.com/Mickey0001/Dixie/" target="_blank" rel="noopener noreferrer" className="btn btn-default DarkGreyBackground" role="button">View Code</a></p>
+                <h3>Software Tools For Easy, Effective Exploratory Testing</h3>
+                <p className="DarkGreyHeading">Ministry of Testing - The Testing Planet 2021</p>
+                <p><a href="https://www.ministryoftesting.com/dojo/lessons/software-tools-for-easy-effective-exploratory-testing" className="btn btn-default LightGreyBackground" target="_blank" rel="noopener noreferrer" role="button">Read the article</a></p>
               </div>
             </div>
           </div>
