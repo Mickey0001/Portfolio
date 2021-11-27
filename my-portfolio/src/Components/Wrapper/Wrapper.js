ae074aa8c8de12f66c25ff3fb8e4765764d5c823
Mirza Sisic <84443026+mirzonius@users.noreply.github.com>
2021-11-27 17:04:28 +0100

    Remove components Clients, Skills, Testimonials Remove SVG Skill Icons Edit header to say Community Leader Edit Wapper component

--- my-portfolio/src/Components/Wrapper/Wrapper.js
+++ my-portfolio/src/Components/Wrapper/Wrapper.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import About from '../../Components/About/About';
-import Clients from '../../Components/Clients/Clients';
 import Resume from '../../Components/Resume/Resume';
 import Projects from '../../Components/Projects/Projects';
-import Testimonials from '../../Components/Testimonials/Testimonials';
-import Skills from '../../Components/Skills/Skills';
 import Contact from '../../Components/Contact/Contact';
 
 const Wrapper = () => (
