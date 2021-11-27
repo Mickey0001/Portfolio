0d2d14956da200c5b0c22959992eec77269426a8
Mirza Sisic <84443026+mirzonius@users.noreply.github.com>
2021-11-27 17:30:42 +0100

    Update Bio

--- my-portfolio/src/Components/About/About.js
+++ my-portfolio/src/Components/About/About.js
@@ -3,18 +3,18 @@ import Mirza from '../../../src/Images/MirzaSisic.png';
 
 const About = () => (
 	<div id="aboutwrap" className="container">
-		<div  className="row">
-			<div  className="col-lg-12">
+		<div className="row">
+			<div className="col-lg-12">
 				<h3 className="heading"><strong>About</strong></h3>
-				<img src={Mirza} alt="Mirza Sisic" id="Mirza"  className="img-fluid"/>
-				<p className="text-justify">I've always been a technology <strong>geek</strong>, friends and family would come to me to solve their computer related problems, so one fine day I  realized that since I love doing it as a hobby why not do it professionally! I decided to drop out of college (I used to study history, I still love reading history books but I love tech even more) and I started fixing PC's on the side. After that, I took a few <a href="http://www.mycertprofile.com/Profile/1947938383" target="_blank" rel="noopener noreferrer">Microsoft Certificates</a> and those helped me land my first full time <strong>IT</strong> job - I got a remote position as a tech support specialist working for a 
-				<a href="http://learninglibrary.com/education" target="_blank" rel="noopener noreferrer"> Canadian e-learning  company</a>.</p> <p className="text-justify"> I worked in tech support for little over two and a half years, while I enjoyed the job,  it felt a little too easy and I needed new challenges to test my mettle. That is when I decided to start learning to code. In January 2017 I discovered <strong>Free Code Camp</strong> and started working on their curriculum. I completed their <a href="https://www.freecodecamp.org/certification/mickey0001/legacy-front-end" target="_blank" rel="noopener noreferrer"> Front-End Development Certification</a> on February 2nd 2018. I'm currently focusing on improving my coding skills, I worked on couple of freelance projects, taking online courses in my free time, going to meetups, blogging and working on my personal projects.</p>
-				<p className="text-justify">Since I started learning web development I completed a lot of tutorials (over 200 hours in <strong>Udemy </strong> courses) but I realized that in order to retain what I've learned I needed to start applying what I've learned. I started small, making a few websites for friends and doing a bit of freelancing on the side. You can check out my <a href="https://github.com/Mickey0001" target="_blank" rel="noopener noreferrer"> GitHub profile</a>  to see the code for the projects I worked on. When I get stuck I resort to <strong>Google-fu</strong> and usually end up finding answers on sites like <strong>StackOverflow</strong>. Occasionally I'll even post a question there (or even answer a question)  - I like to live dangerously!</p><p className="text-justify"> Another helpful resource was the RealToughCandy online community, we had chat room full of friendly nerds willing to help each other out, also there is a thing called the <strong>Mentorship Mondays</strong> where we would meet weekly via Google Hangouts to learn together! Also <a href="http://www.100daysofcode.com/" target="_blank" rel="noopener noreferrer"> #100DaysOfCode </a> - a movement where you code every day for 100 days straight has been very motivating and helpful.</p>
+				<img src={Mirza} alt="Mirza Sisic" id="Mirza" className="img-fluid" />
+				<p className="text-justify">I've always been a technology <strong>geek</strong>, friends and family would come to me to solve their computer related problems, so one fine day I  realized that since I love doing it as a hobby why not do it professionally! I decided to drop out of college (I used to study history, I still love reading history books but I love tech even more) and I started fixing PC's on the side. After that, I took a few Microsoft Certificates and those helped me land my first full time <strong>IT</strong> job - I got a remote position as a tech support specialist working for a
+					Canadian e-learning  company.</p> <p className="text-justify"> I worked in tech support for little over two and a half years, while I enjoyed the job,  it felt a little too easy and I needed new challenges to test my mettle. That is when I decided to start transitioning to software testing and also started to learn coding. In my free time I'm, taking online courses, attending and organizing community meetups, blogging, working on my personal projects and mentoring junior testers.</p>
 				<p className="text-justify">
-				I also have a technical blog, called <a href="https://juniorlearnstocode.blogspot.ba/" target="_blank" rel="noopener noreferrer"> Junior Learns To Code </a> where I share my thoughts and describe my learning journey.</p>
-				<p className="text-justify"><em>Currently I'm primarily focusing on learning more about QA test automation, getting better with ASP.NET/C#, SQL, NUnit, Selenium, Visual Studio, API testing and Microsoft tech stack in general, very interesed in Auze (Cloud in general) as well!</em></p>
- 			</div>
- 		</div>
- 	</div>
+					I'm the organizer and founder of the <a href="https://www.meetup.com/ministry-of-testing-sarajevo/" target="_blank" rel="noopener noreferrer">Ministry of Testing Sarajevo</a> community meetup. When I get stuck I resort to <strong>Google-fu</strong> and usually end up finding answers on sites like <strong>MoT Club</strong> forum and <strong>StackOverflow</strong>.
+					I write a technical blog, called <a href="https://juniorlearnstocode.blogspot.ba/" target="_blank" rel="noopener noreferrer"> Junior Learns To Code </a> where I share my thoughts and describe my learning journey.</p>
+				<p className="text-justify"><em>Currently I'm primarily focusing on improving my testing skills, getting better at automated checking, doing quality coaching, focusing on the community aspects of software testing and spamming people with memes.</em></p>
+			</div>
+		</div>
+	</div>
 );
 export default About;
