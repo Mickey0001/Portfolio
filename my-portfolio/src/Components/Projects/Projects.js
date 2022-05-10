ab13fbc2859aae0607304a73d607e8ede613b397
Mirza Sisic <mirzasisic57@gmail.com>
2022-05-10 16:50:11 +0200

    Updated activities section with new content

--- my-portfolio/src/Components/Projects/Projects.js
+++ my-portfolio/src/Components/Projects/Projects.js
@@ -4,6 +4,15 @@ import ExploratoryTools from '../../../src/Images/ExploratoryToolsForBetterTesti
 import MisconceptionsExploratory from '../../../src/Images/CommonMisconceptionsAboutExploratoryTesting.jpg';
 import ChallengesInTestingLegacySystems from '../../../src/Images/ChallengesInTestingLegacySystems.png';
 import TestingAMA from '../../../src/Images/MotAmaTestingToolsWithMirzaSisic.png';
+import ModernTestingPrinciples from '../../../src/Images/ModernTestingPrinciples.jpg';
+import OrganizingYourVeryOwnTestingMeetup from '../../../src/Images/OrganizingYourVeryOwnTestingMeetup.jpg';
+import ModernTestingBestPractices from '../../../src/Images/ModernTestingBestPractices.jpg';
+import TheRoleOfQaInSoftwareDevelopment from '../../../src/Images/TheRoleOfQaInSoftwareDevelopment.jpg';
+import MotSarajevo1 from '../../../src/Images/MotSarajevo1.jpg';
+import iSQI from '../../../src/Images/iSQI.jpg';
+import GeekleModerationCert from '../../../src/Images/GeekleModerationCert.png';
+
+
 
 const Projects = () => (
   <div className="container" id="workwrap">
@@ -58,8 +67,104 @@ const Projects = () => (
           </div>
         </div>
       </div>
-
     </div>
+
+    <div className="col-sm-6 col-md-3">
+      <div className="thumbnail">
+        <img className="img-responsive project" src={ModernTestingPrinciples} alt="Modern Testing Principles - blog article for TestProject, by Mirza Sisic" />
+        <div className="caption">
+          <h3>Modern Testing Principles</h3>
+          <p className="DarkGreyHeading">TestProject - Blog Article</p>
+          <span className="spacer"></span>
+          <p><a href="https://blog.testproject.io/2022/04/21/modern-testing-principles/" className="btn btn-default LightGreyBackground" target="_blank" rel="noopener noreferrer" role="button">Read the article</a></p>
+        </div>
+      </div>
+    </div>
+
+    <div className="col-sm-6 col-md-3">
+      <div className="thumbnail">
+        <img className="img-responsive project" src={ChallengesInTestingLegacySystems} alt="Challenges in Testing Legacy Systems - blog article for TestProject, by Mirza Sisic" />
+        <div className="caption">
+          <h3>Challenges in Testing Legacy Systems</h3>
+          <p className="DarkGreyHeading">TestProject - Blog Article</p>
+          <span className="spacer"></span>
+          <p><a href="https://blog.testproject.io/2021/08/16/challenges-in-testing-legacy-systems/" className="btn btn-default LightGreyBackground" target="_blank" rel="noopener noreferrer" role="button">Read the article</a></p>
+        </div>
+      </div>
+    </div>
+
+    <div className="col-sm-6 col-md-3">
+      <div className="thumbnail">
+        <img className="img-responsive project" src={OrganizingYourVeryOwnTestingMeetup} alt="Organizing Your Very Own Testing Meetup - article" />
+        <div className="caption">
+          <h3>Organizing Your Very Own Testing Meetup</h3>
+          <p className="DarkGreyHeading">Ministry of Testing - The Testing Planet 2022</p>
+          <span className="spacer"></span>
+          <p><a href="https://www.ministryoftesting.com/dojo/lessons/organizing-your-very-own-testing-meetup" className="btn btn-default LightGreyBackground" target="_blank" rel="noopener noreferrer" role="button">Read the article</a></p>
+        </div>
+      </div>
+    </div>
+
+    <div className="col-sm-6 col-md-3">
+      <div className="thumbnail">
+        <img className="img-responsive project" src={ModernTestingBestPractices} alt="Modern Testing Best Practices with Venkat Ramakrishnan - online meetup" />
+        <div className="caption">
+          <h3>Modern Testing Best Practices</h3>
+          <p className="DarkGreyHeading">Ministry of Testing Sarajevo - Online Meetup</p>
+          <span className="spacer"></span>
+          <p><a href="https://www.ministryoftesting.com/dojo/lessons/organizing-your-very-own-testing-meetup" className="btn btn-default LightGreyBackground" target="_blank" rel="noopener noreferrer" role="button">Watch the Recording</a></p>
+        </div>
+      </div>
+    </div>
+
+    <div className="col-sm-6 col-md-3">
+      <div className="thumbnail">
+        <img className="img-responsive project" src={TheRoleOfQaInSoftwareDevelopment} alt="The role of QA in software development with Velibor Mandžo - online meetup" />
+        <div className="caption">
+          <h3>The role of QA in software development</h3>
+          <p className="DarkGreyHeading">Ministry of Testing Sarajevo - Online Meetup</p>
+          <span className="spacer"></span>
+          <p><a href="https://www.ministryoftesting.com/dojo/series/meetups/lessons/the-role-of-qa-in-software-development" className="btn btn-default LightGreyBackground" target="_blank" rel="noopener noreferrer" role="button">Watch the Recording</a></p>
+        </div>
+      </div>
+    </div>
+
+    <div className="col-sm-6 col-md-3">
+      <div className="thumbnail">
+        <img className="img-responsive project" src={MotSarajevo1} alt="MoT Sarajevo's First Meetup with Maaret Pyhäjärvi, Conor Fitzgerald and Larry Goddard - online meetup" />
+        <div className="caption">
+          <h3>MoT Sarajevo's First Meetup</h3>
+          <p className="DarkGreyHeading">Ministry of Testing Sarajevo - Online Meetup</p>
+          <span className="spacer"></span>
+          <p><a href="https://www.ministryoftesting.com/dojo/series/meetups/lessons/mot-sarajevo-s-first-meetup" className="btn btn-default LightGreyBackground" target="_blank" rel="noopener noreferrer" role="button">Watch the Recording</a></p>
+        </div>
+      </div>
+    </div>
+
+    <div className="col-sm-6 col-md-3">
+      <div className="thumbnail">
+        <img className="img-responsive project" src={iSQI} alt="MoT Sarajevo's First Meetup with Maaret Pyhäjärvi, Conor Fitzgerald and Larry Goddard - online meetup" />
+        <div className="caption">
+          <h3>Open Discussion ISTQB Certification: where to start?</h3>
+          <p className="DarkGreyHeading">Ministry of Testing Sarajevo - Online Meetup</p>
+          <span className="spacer"></span>
+          <p><a href="https://www.ministryoftesting.com/dojo/series/meetups/lessons/mot-sarajevo-s-first-meetup" className="btn btn-default LightGreyBackground" target="_blank" rel="noopener noreferrer" role="button">Watch the Recording</a></p>
+        </div>
+      </div>
+    </div>
+
+    <div className="col-sm-6 col-md-3">
+      <div className="thumbnail">
+        <img className="img-responsive project" src={GeekleModerationCert} alt="MoT Sarajevo's First Meetup with Maaret Pyhäjärvi, Conor Fitzgerald and Larry Goddard - online meetup" />
+        <div className="caption">
+          <h3>QA Global Summit 2022</h3>
+          <p className="DarkGreyHeading">Geekle - Online Conference</p>
+          <span className="spacer"></span>
+          <p><a href="https://youtu.be/RVDXWiqFX0M?t=594" className="btn btn-default LightGreyBackground" target="_blank" rel="noopener noreferrer" role="button">Watch the Recording</a></p>
+        </div>
+      </div>
+    </div>
+
   </div>
 );
 
